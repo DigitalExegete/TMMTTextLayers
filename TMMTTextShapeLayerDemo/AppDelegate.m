2bd9b0052d4e1cb17651528d05959fc63c0f4ced
William T. Jones <digitalexegete@gmail.com>
2014-12-21 14:45:40 -0600

    Updating The Schemes to have a better project name

--- TMMTTextShapeLayerDemo/AppDelegate.m
+++ TMMTTextShapeLayerDemo/AppDelegate.m
@@ -24,14 +24,26 @@
 @property (retain) IBOutlet NSPanel *textInputPanel;
 @property (assign) BOOL showWindow;
 @property (retain) TMMTTextLayer *textLayer;
+@property (copy) NSString *zoomString;
+
 @end
 
 @implementation AppDelegate
 
-
+-(void)dealloc
+{
+	[_textInputPanel release];
+	[_textLayer release];
+	[_zoomString release];
+	[super dealloc];
+	
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	// Insert code here to initialize your application
+	
+	[self.scrollView addObserver:self forKeyPath:@"magnification" options:NSKeyValueObservingOptionNew context:nil];
+	
 	self.showWindow = NO;
 	TMMTTextLayer *newTextLayer = [TMMTTextLayer layer];
 	self.textLayer = newTextLayer;
@@ -41,15 +53,45 @@
 	[self.layerView setWantsLayer:YES];
 	[newTextLayer setBackgroundColor:[[NSColor whiteColor] CGColor]];
 	[newTextLayer setDelegate:newTextLayer];
-	[newTextLayer setFrame:NSMakeRect(10, 50, 640, 480)];
+	[newTextLayer setFrame:NSMakeRect(10, 50, 160, 30)];
 	[newTextLayer setParagraphAlignment:NSCenterTextAlignment];
-	[newTextLayer setBorderWidth:1];
+	[newTextLayer setBorderWidth:0];
 	[self.layerView setFrame:self.layerView.superview.bounds];
 	[[self.layerView layer] addSublayer:newTextLayer];
 	newTextLayer.textFont = [NSFont systemFontOfSize:16];//[NSFont fontWithName:@"Helvetica Neue" size:16];
-	[newTextLayer setString:@"2\nCLASP"];
+	[newTextLayer setString:@"TMMTTextLayer"];
+	
+	self.scrollView.magnification = 1.0;
+	self.zoomString = @"Zoom: 1.0";
+	
+	CATextLayer *regularTextLayer = [CATextLayer layer];
+	[regularTextLayer setBackgroundColor:[[NSColor whiteColor] CGColor]];
+	[regularTextLayer setForegroundColor:[[NSColor blackColor] CGColor]];
+	[regularTextLayer setDelegate:regularTextLayer];
+	[regularTextLayer setFrame:NSMakeRect(10,80, 160, 30)];
+	[regularTextLayer setAlignmentMode:kCAAlignmentCenter];
+	[regularTextLayer setFont:(CTFontRef)[NSFont systemFontOfSize:12]];
+	[regularTextLayer setFontSize:16];
+	[regularTextLayer setBorderWidth:0];
+	[regularTextLayer setString:@"CATextLayer"];
+	[self.layerView.layer addSublayer:regularTextLayer];
+	[regularTextLayer setNeedsDisplay];
+	
+	
 	
 
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+	
+	if ([keyPath isEqualToString:@"magnification"])
+	{
+		self.zoomString = [NSString stringWithFormat:@"Zoom: %2.1f",self.scrollView.magnification];
+	}
+	else
+		[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+	
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
